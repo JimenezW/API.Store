b78dc5437487ad954fd9b78759b885b6a52c8832
walter j <jimenezjimenezw@gmail.com>
2023-05-15 23:05:28 -0600

    Agregar archivos de proyecto.

---
+++ API.Store.Data/APIStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using API.Store.Shared;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Store.Data
+{
+    public class APIStoreContext : DbContext
+    {
+        public APIStoreContext(DbContextOptions Options):base(Options)
+        {
+            
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+    }
+}
