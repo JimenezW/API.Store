8fd6c04098312f85731fcc7c0eaa447bfa89eb96
walter j <jimenezjimenezw@gmail.com>
2023-05-18 23:15:35 -0600

    implementacion endpoint login, modificacion controller Authorize y activacion de JWT para swagger

--- API.Store.API/Controllers/AuthenticationController.cs
+++ API.Store.API/Controllers/AuthenticationController.cs
@@ -48,7 +48,7 @@ namespace API.Store.API.Controllers
                 UserName = request.Name
             };
 
-            var isCreated = await _userManager.CreateAsync(user);
+            var isCreated = await _userManager.CreateAsync(user, request.Password);
 
             if (isCreated.Succeeded) 
             {
@@ -59,25 +59,50 @@ namespace API.Store.API.Controllers
                     Token = token
                 });
             }
-            else
-            {
-                var errors = new List<string>();
-                foreach (var err in isCreated.Errors)
-                    errors.Add(err.Description);
 
-                return BadRequest(new AuthResult() {
-                    Result = false,
-                    Errors = errors
-                });
-            }
+            var errors = new List<string>();
+            foreach (var err in isCreated.Errors)
+                errors.Add(err.Description);
 
             return BadRequest(new AuthResult()
             {
                 Result = false,
-                Errors = new List<string>
+                Errors = errors.Count == 0 ? errors : new List<string> {"User couldn't be created"}
+            });
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginResquestDtos dto)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+
+            var existingUser =  await _userManager.FindByEmailAsync(dto.Email);
+            if (existingUser == null)
+            {
+                return BadRequest(new AuthResult
                 {
-                    "User couldn't be created"
-                }
+                    Errors = new List<string> { "Invalid payload" },
+                    Result = false
+                });
+            }
+
+            var checkUserAndPass = await _userManager.CheckPasswordAsync(existingUser, dto.Password);
+            if (!checkUserAndPass)
+            {
+                return BadRequest(new AuthResult
+                {
+                    Errors = new List<string> { "Invalid credentials" },
+                    Result = false
+                });
+            }
+
+            var token = GenerateToken(existingUser);
+            return Ok(new AuthResult()
+            {
+                Result = true,
+                Token = token
             });
         }
 
--- API.Store.API/Controllers/OrdersController.cs
+++ API.Store.API/Controllers/OrdersController.cs
@@ -1,11 +1,13 @@
 ﻿using API.Store.Data;
 using API.Store.Shared;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Store.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class OrdersController : ControllerBase
